bd6d6442c8e62ff793efd19f63f566267af6ab0d
sammtcbn <sammtcbn@gmail.com>
2021-12-29 11:52:51 +0800

    show more msg while install

--- microk8s-install.sh
+++ microk8s-install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 mkdir ~/.kube
+#sudo snap install microk8s --classic --channel=1.21
 sudo snap install microk8s --classic
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
-
-sudo microk8s status --wait-ready
 
 sudo microk8s enable storage dns helm3
 #sudo microk8s enable ingress
@@ -15,11 +14,21 @@ sudo microk8s enable storage dns helm3
 
 #newgrp microk8s
 
+sudo microk8s status --wait-ready
+
+#echo "alias kubectl='microk8s kubectl'" >> ~/.bash_aliases
+
 sudo snap alias microk8s.kubectl kubectl
 sudo snap alias microk8s.helm3 helm
 
 echo microk8s status
 microk8s status
 
-echo microk8s kubectl get pod -A
-microk8s kubectl get pod -A
+echo kubectl get nodes
+kubectl get nodes
+
+echo kubectl get pod -A
+kubectl get pod -A
+
+echo kubectl get services -A
+kubectl get services -A
