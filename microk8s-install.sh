e77bf31a66005533a6f048acb0b41b3b3cf8bb4c
sammtcbn <sammtcbn@gmail.com>
2021-12-29 23:40:49 +0800

    reload the user groups by running newgrp microk8s

--- microk8s-install.sh
+++ microk8s-install.sh
@@ -6,21 +6,20 @@ sudo snap install microk8s --classic
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
 
-sudo microk8s enable storage dns helm3
-#sudo microk8s enable ingress
-#sudo microk8s enable dashboard
-
-#sudo microk8s enable portainer
-# portainer default por is 30777 , default user is admin
-
-#newgrp microk8s
-
-sudo microk8s status --wait-ready
-
 #echo "alias kubectl='microk8s kubectl'" >> ~/.bash_aliases
-
 sudo snap alias microk8s.kubectl kubectl
 sudo snap alias microk8s.helm3 helm
+
+newgrp microk8s
+
+microk8s enable storage dns helm3
+#microk8s enable ingress
+#microk8s enable dashboard
+
+#microk8s enable portainer
+# portainer default por is 30777 , default user is admin
+
+microk8s status --wait-ready
 
 echo microk8s status
 microk8s status
