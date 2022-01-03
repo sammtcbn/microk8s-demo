bcaf8b75d3f5d55e565114632e6f550c07877aca
sammtcbn <sammtcbn@gmail.com>
2022-01-03 19:30:36 +0800

    add script for show information

--- show-info.sh
+++ show-info.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 echo microk8s status
-sudo microk8s status
+microk8s status
+echo
+
+echo microk8s.config
+microk8s.config
 echo
 
 echo kubectl version
-sudo kubectl version
+kubectl version
 echo
 
 echo helm version
-sudo helm version
+helm version
 echo
 
 echo kubectl get nodes
-sudo kubectl get nodes
+kubectl get nodes
 echo
 
 echo kubectl get pod -A
-sudo kubectl get pod -A
+kubectl get pod -A
 echo
 
 echo kubectl get services -A
-sudo kubectl get services -A
+kubectl get services -A
 echo
 
 echo kubectl get all --all-namespaces
-sudo kubectl get all --all-namespaces
+kubectl get all --all-namespaces
 echo
 
 echo kubectl cluster-info
-sudo kubectl cluster-info
+kubectl cluster-info
 echo
 
