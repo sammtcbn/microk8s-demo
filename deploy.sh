a2ca35274c1112bc0e66d66498dc89135cd6b635
sammtcbn <sammtcbn@gmail.com>
2022-01-06 17:42:45 +0800

    deploy by helm pull

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,19 @@
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-BITNAMI_PATH=${SCRIPT_PATH}/bitnami-charts
+helm repo add bitnami https://charts.bitnami.com/bitnami
 
-cd ${BITNAMI_PATH}/bitnami/rabbitmq || exit 1
+cd ${SCRIPT_PATH} || exit 1
+helm pull bitnami/rabbitmq --untar
+cd ${SCRIPT_PATH}/rabbitmq || exit 1
 helm install rabbitmq -f values.yaml bitnami/rabbitmq
 
-cd ${BITNAMI_PATH}/bitnami/postgresql || exit 1
+cd ${SCRIPT_PATH} || exit 1
+helm pull bitnami/postgresql --untar
+cd ${SCRIPT_PATH}/postgresql || exit 1
 helm install postgresql -f values.yaml bitnami/postgresql
 
-cd ${BITNAMI_PATH}/bitnami/mongodb || exit 1
+cd ${SCRIPT_PATH} || exit 1
+helm pull bitnami/mongodb --untar
+cd ${SCRIPT_PATH}/mongodb || exit 1
 helm install mongodb -f values.yaml bitnami/mongodb
