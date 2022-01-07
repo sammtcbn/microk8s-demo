f676ba91ffd8324d8bfbfc9b43a711f5d66bf8f2
sammtcbn <sammtcbn@gmail.com>
2022-01-07 18:42:41 +0800

    reorg scripts

--- undeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-BITNAMI_PATH=${SCRIPT_PATH}/bitnami-charts
-
-helm uninstall mongodb
-helm uninstall postgresql
