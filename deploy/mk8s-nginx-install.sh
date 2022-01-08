1c7071404e81c37a7a1eac86399bdbdaa8ec79fe
sammtcbn <sammtcbn@gmail.com>
2022-01-08 18:42:43 +0800

    reorg script

--- deploy/mk8s-nginx-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo kubectl create deployment nginx --image=nginx
-kubectl create deployment nginx --image=nginx
-echo
-
-echo kubectl expose deployment nginx --type=ClusterIP --port=80 --name=nginx-service
-kubectl expose deployment nginx --type=ClusterIP --port=80 --name=nginx-service
-echo
