135e3eb1f210d9c485523e74ec532b658b8abd72
sammtcbn <sammtcbn@gmail.com>
2022-01-08 13:44:48 +0800

    install microk8s 1.23 stable

--- microk8s-install.sh
+++ microk8s-install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 mkdir -p ~/.kube
+
+#sudo snap install microk8s --classic
 #sudo snap install microk8s --classic --channel=1.21
 #sudo snap install microk8s --classic --channel=1.22/stable
-sudo snap install microk8s --classic
+sudo snap install microk8s --classic --channel=1.23/stable
+
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
 
