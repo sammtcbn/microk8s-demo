551b12fe9b64c9c6d7de2d797e7b7be6d350a208
sammtcbn <sammtcbn@gmail.com>
2022-01-20 16:31:35 +0800

    snap unalias while uninstall

--- microk8s-uninstall.sh
+++ microk8s-uninstall.sh
@@ -4,12 +4,12 @@ sudo microk8s disable storage dns helm3
 sudo microk8s reset
 sudo microk8s stop
 
+sudo snap unalias kubectl
+sudo snap unalias helm
+
 sudo snap disable microk8s
 
 #sudo snap remove microk8s
 sudo snap remove --purge microk8s
 
 rm -rf ~/.kube
-
-sudo snap unalias kubectl
-sudo snap unalias helm
